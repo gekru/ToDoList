419d9a7d12b8c85b33801c5e1e06ce420a3a6132
Gennadii <mail@mail.com>
2020-06-21 12:37:50 +0300

    New model ToDo.cs added

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        /// <summary>
+        /// The unique Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Description of ToDo's 
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Show if ToDo done or not
+        /// </summary>
+        public bool IsDone { get; set; }
+    }
+}
